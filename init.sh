6cf98d454870b3d444a7bd5d69b233bee253f65e
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2022-03-09 10:29:31 +0000

    Add README.md to init.sh

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ project_copyright_description="example_copyright_description"  # Appears in copy
 
 # === STOP HERE ========
 
-for file in setup.cfg src/PROJECT_NAME/__init__.py docs/source/conf.py docs/source/index.rst docs/source/PROJECT_NAME.rst; do
+for file in README.md setup.cfg src/PROJECT_NAME/__init__.py docs/source/conf.py docs/source/index.rst docs/source/PROJECT_NAME.rst; do
 	sed -i "s/PROJECT\\\\\\?_NAME/$project_name/g" $file
 	sed -i "s/AUTHOR_NAMES/$author_names/g" $file
 	sed -i "s/PROJECT_DESCRIPTION/$project_description/g" $file
