1447b2219b8ad8e9c07cf9349dd6317bb9d189ca
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2022-03-09 23:46:08 +0000

    Update __init__.py text

--- src/PROJECT_NAME/__init__.py
+++ src/PROJECT_NAME/__init__.py
@@ -4,6 +4,6 @@
 # This program is licensed under GNU GPLv3, available here:
 # <https://www.gnu.org/licenses/gpl-3.0.html>
 
-"""This is the top-level ``PROJECT_NAME`` package, which contains all the subpackages of the project."""
+"""This is the top-level ``PROJECT_NAME`` package, which contains all the subpackages and submodules of the project."""
 
 __version__ = '0.0.1-alpha'
