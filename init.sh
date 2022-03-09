46ae1ea45b8b16891832ca4e8e366091b60b3663
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2022-03-09 16:50:23 +0000

    Add more info to setup.cfg

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@
 
 project_name="example_name"
 author_names="example_names"
+github_name="example_github_name"
 project_description="example_description"  # Appears in setup.cfg
 project_copyright_description="example_copyright_description"  # Appears in copyright comments
 
@@ -12,6 +13,7 @@ project_copyright_description="example_copyright_description"  # Appears in copy
 for file in README.md setup.cfg src/PROJECT_NAME/__init__.py docs/source/conf.py docs/source/index.rst docs/source/PROJECT_NAME.rst; do
 	sed -i "s/PROJECT\\\\\\?_NAME/$project_name/g" $file
 	sed -i "s/AUTHOR_NAMES/$author_names/g" $file
+	sed -i "s/GITHUB_NAME/$github_name/g" $file
 	sed -i "s/PROJECT_DESCRIPTION/$project_description/g" $file
 	sed -i "s/PROJECT_COPYRIGHT_DESCRIPTION/$project_copyright_description/g" $file
 done
